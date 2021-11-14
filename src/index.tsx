1f6dd0987c4102b3a64dafaa0027219721b2e67e
Gabriel Alves <gabrieltots@gmail.com>
2021-11-14 20:10:44 -0300

    application cleaned

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export function AppRoot() {
+    return (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text>Hello!</Text>
+        </View>
+    )
+};
